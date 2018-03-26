68eeb3146c68f8116fc45d4ed8ef413a7fcf0ee9
Cuneyt Akcay <cuneytakcay@gmail.com>
2018-03-26 15:09:51 -0700

    modified to for es6

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  state= { developers: [] }
+  state = { developers: [] }
 
   componentDidMount() {
     fetch('/users')
@@ -18,11 +18,9 @@ class App extends Component {
           <h1 className="App-title">Skill Share App</h1>
           <h2>Development Team</h2>
         </header>
-        <ul>
-          {this.state.developers.map(developer => 
-            <li key={developer.id}>{developer.firstName} {developer.lastName}</li>
-          )}
-        </ul>
+        {this.state.developers.map(developer => 
+          <div key={developer.id}>{developer.name}</div>
+        )}
       </div>
     );
   }
--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  // And insert something like this instead:
+router.get('/', (req, res, next) => {
   res.json([{
-  	id: 1,
-  	firstName: "Cuneyt",
-  	lastName: "Akcay"
-  }, {
-  	id: 2,
-  	firstName: "Jason",
-  	lastName: "Leo"
-  }, {
-  	id: 3,
-  	firstName: "Chris",
-  	lastName: "Rolls"
-  }, {
-  	id: 4,
-  	firstName: "Roch",
-  	lastName: "Mirabeau"
-  }]);
+		id: 1, name: "Cuneyt Akcay"
+	}, {
+		id: 2, name: "Jason Leo"
+	}, {
+		id: 3, name: "Chris Rolls"
+	}, {
+		id: 4, name: "Roch Mirabeau"
+	}]);
 });
 
 module.exports = router;
